9e5ff91ada8ebe8ce779108a61cd45e7a722cda5
vishwas babar <vishwasbabar886@gmail.com>
2024-01-28 09:14:00 +0530

    done tailwind setup

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./views/**/*.html', './public/css/*.css', './public/js/*.js'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
